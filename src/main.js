cd64bb217d9bee38675eaed0a7784b5d2061d72d
Nin10820 <Minhnguyen1082000@gmail.com>
2020-07-24 10:48:22 +0700

    Testimonials

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 
 AOS.init();
